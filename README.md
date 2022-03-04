940f7a2f29db3e27eb325d7d3705dd273e98c384
조호진 <hojin@innowireless.co.kr>
2022-03-04 20:42:40 +0900

    초기구성

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# aggrid
+# vuejs
+
+## 환경
+
+* 개발툴 - Visual Studio Code 사용
+
+* vue cli를 통한 프로젝트 생성  
+https://cli.vuejs.org/guide/  
+vue cli 설치  
+`npm install -g @vue/cli`  
+vue cli 업데이트  
+`npm update -g @vue/cli`  
+vue 프로젝트 생성(상위 디렉토리에서 실행, 기본 구성)  
+`vue create vuejs`
+
+* ag grid 설치
+`npm install --save ag-grid-community ag-grid-vue vue-property-decorator@^8.0.0`  
+
+## 실행  
+package.json의 scripts를 통해서 실행  
+serve - 웹브라우저를 띄워주면, 수정 시 자동 반영 됨  
+
+## 참조  
+https://www.ag-grid.com/vue-data-grid/getting-started/  
+
+
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <ag-grid-vue id="grid" class="ag-theme-alpine"
+    :columnDefs="columnDefs"
+    :rowData="rowData">
+    
+  </ag-grid-vue>
+
+</template>
+
+<script>
+import {AgGridVue} from "ag-grid-vue";
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    AgGridVue
+  },
+  data() {
+    return {
+      columnDefs: null,
+      rowData : null
+    }
+  },
+  props: {
+    msg: String
+  },
+  beforeMount() {
+    this.columnDefs = [
+      { field: "make" },
+      { field: "model" },
+      { field: "price" },      
+    ];
+
+    this.rowData = [
+      { make: "Toyota", model: "Celica", price: 35000 },
+      { make: "Ford", model: "Mondeo", price: 32000 },
+      { make: "Porsche", model: "Boxter", price: 72000 },
+    ];
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  #grid {
+    width: 700px;
+    height: 500px;
+  }
+
+
+</style>
